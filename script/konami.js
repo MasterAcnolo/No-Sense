7b71f7c4f1dd77c56fc1c4f4a0233261cdd400d0
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-06-12 09:49:38 +0200

    Push

--- script/konami.js
+++ script/konami.js
@@ -9,7 +9,7 @@ window.addEventListener('keydown', function(e) {
   if (e.key === konamiCode[konamiIndex]) {
     konamiIndex++;
     if (konamiIndex === konamiCode.length) {
-      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      window.location.href = "https://www.youtube-nocookie.com/v/dQw4w9WgXcQ?autoplay=1";
       konamiIndex = 0;
     }
   } else {

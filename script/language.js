97768de9208ce335b788342ddd78020a4273c192
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-08-04 13:09:45 +0200

    Lyrics Page  + Back link Fix

---
+++ script/language.js
@@ -0,0 +1,28 @@
+async function changeLanguage(lang) {
+    try {
+        const response = await fetch(`/script/lang/${lang}.json`);
+        const translations = await response.json();
+
+        for (const key in translations) {
+            const elements = document.querySelectorAll(`[data-i18n="${key}"]`);
+            elements.forEach(el => {
+                el.textContent = translations[key];
+            });
+        }
+    } catch (error) {
+        console.error("Erreur de chargement de la langue :", error);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    changeLanguage('ru');
+
+    const languages = ['fr', 'en', 'ru', 'ru-romanified'];
+
+    languages.forEach(lang => {
+        const button = document.getElementById(`${lang}-button`);
+        if (button) {
+            button.addEventListener('click', () => changeLanguage(lang));
+        }
+    });
+});
--- script/loader.js
+++ script/loader.js
@@ -1,5 +1,6 @@
  window.addEventListener("load", () => {
         const loader = document.querySelector(".loader");
+        document.body.classList.remove("loading");
         loader.classList.add("fade-out");
 
         setTimeout(() => {
@@ -12,39 +13,3 @@
 async function alertSpotify(){
   alert("Sorry, not working actually :(")
 }
-
-// document.querySelector('.spotify-button').addEventListener('click', function () {
-//   const container = this.parentElement;
-
-//   // Nettoie le container
-//   container.innerHTML = '';
-
-//   // Crée l'iframe Spotify
-//   const iframe = document.createElement('iframe');
-//   iframe.src = "https://open.spotify.com/embed/track/2fIBmScNzkGmSJ3y2XsmEI?theme=white&view=coverart";
-//   iframe.width = "80%";
-//   iframe.height = "100%";
-//   iframe.frameBorder = "0";
-//   iframe.allow = "encrypted-media";
-//   iframe.loading = "lazy";
-//   iframe.style.borderRadius = "10px";
-//   iframe.className = "spotify-iframe";
-
-//   // Crée un overlay de chargement
-//   const overlay = document.createElement('div');
-//   overlay.className = 'spotify-loader-overlay';
-//   overlay.innerHTML = `
-//     <div class="loader-circle"></div>
-//     <p>Loading Spotify…</p>
-//   `;
-
-//   // Ajoute tout
-//   container.appendChild(iframe);
-//   container.appendChild(overlay);
-
-//   // Enlève l’overlay après un délai (2 secondes ici)
-//   setTimeout(() => {
-//     overlay.classList.add('hide');
-//     setTimeout(() => overlay.remove(), 500); // supprime du DOM après l’anim
-//   }, 2000);
-// });
